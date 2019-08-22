10f618ee2bcd32262e6283d319e2ee07f5e5893b
Svetla <svetla.syrimis@gmail.com>
2019-08-22 09:26:23 -0400

    final

--- client-ohn/src/services/api-helper.js
+++ client-ohn/src/services/api-helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
-// const baseUrl = 'https://blooming-tundra-33956.herokuapp.com'
+// const baseUrl = 'http://localhost:3000'
+const baseUrl = 'https://blooming-tundra-33956.herokuapp.com'
 
 
 export const api = axios.create({
